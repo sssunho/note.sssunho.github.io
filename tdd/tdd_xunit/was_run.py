a7c817278c3d81b05d7b228db4e84604d36de9bc
sssunho <sssunho.lee@gmail.com>
2020-01-19 18:28:54 +0900

    TDD: 23장. 얼마나 달콤한지
    - TestSuite를 위한 테스트를 작성
    - 테스트를 통과시키지 못한 채 일부분만 구현했다. 이것은 '규칙'
    위반이다. 만약 그때 이걸 직접 발견했다면, 돈주머니에서 테스트 케이스 두
    개를 공짜로 가져가도 좋다. 테스트를 통과시키고 초록 막대 상태에서
    리팩토링할 수 있게 할 간단한 가짜 구현이 있을 것 같긴 한데, 지금은 그게
    뭔지 잘 떠오르지 않는다.
    - 아이템과 아이템의 모음(컴포지트)이 동일하게 작동할 수 있도록 run
    메서드의 인터페이스를 변경하였고 마침내 테스트를 통과시켰다.
    - 공통된 셋업 코드를 분리했다.

--- tdd/tdd_xunit/was_run.py
+++ tdd/tdd_xunit/was_run.py
@@ -3,10 +3,12 @@
 [O] 먼저 setUp 호출하기
 [O] 나중에 tearDown 호출하기
 [] 테스트 메서드가 실패하더라도 tearDown 호출하기
-[] 여러 개의 테스트 실행하기
+[O] 여러 개의 테스트 실행하기
 [O] 수집된 결과를 출력하기
 [O] wasRun에 로그 문자열 남기기
 [O] 실패한 테스트 보고하기
+[] setup 에러를 잡아서 보고하기
+[] TestCase 클래스에서 TestSuite 생성하기
 """
 
 
@@ -17,8 +19,7 @@ class TestCase:
     def setup(self):
         pass
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.test_started()
 
         self.setup()
@@ -28,8 +29,6 @@ class TestCase:
         except:
             result.test_failed()
         self.teardown()
-
-        return result
 
     def teardown(self):
         pass
@@ -67,31 +66,60 @@ class TestResult:
         return '{} run, {} failed'.format(self.runCount, self.failureCount)
 
 
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
+
 class TestCaseTest(TestCase):
+    def setup(self):
+        self.result = TestResult()
+
     def test_template_method(self):
         test = WasRun('test_method')
-        test.run()
+        test.run(self.result)
         assert 'setup test_method teardown ' == test.log
 
     def test_result(self):
         test = WasRun('test_method')
-        result = test.run()
-        assert '1 run, 0 failed' == result.summary()
+        test.run(self.result)
+        assert '1 run, 0 failed' == self.result.summary()
 
     def test_failed_result(self):
         test = WasRun('test_broken_method')
-        result = test.run()
-        assert '1 run, 1 failed' == result.summary()
+        test.run(self.result)
+        assert '1 run, 1 failed' == self.result.summary()
 
     def test_failed_result_formatting(self):
-        result = TestResult()
         result.test_started()
         result.test_failed()
-        assert '1 run, 1 failed' == result.summary()
+        assert '1 run, 1 failed' == self.result.summary()
+
+    def test_suite(self):
+        suite = TestSuite()
+        suite.add(WasRun('test_method'))
+        suite.add(WasRun('test_broken_method'))
+
+        #: 호출하는 곳에서 TestResults를 할당하는 전략
+        # run()이 명시적으로 반환하지 않아도 된다는 장점이 있음
+        suite.run(self.result)
+        assert '2 run, 1 failed' == self.result.summary()
 
 
 if __name__ == '__main__':
-    TestCaseTest('test_template_method').run()
-    TestCaseTest('test_result').run()
-    TestCaseTest('test_failed_result_formatting').run()
-    TestCaseTest('test_failed_result').run()
+    suite = TestSuite()
+    suite.add(TestCaseTest('test_template_method'))
+    suite.add(TestCaseTest('test_result'))
+    suite.add(TestCaseTest('test_failed_result_formatting'))
+    suite.add(TestCaseTest('test_failed_result'))
+    suite.add(TestCaseTest('test_suite'))
+    result = TestResult()
+    suite.run(result)
+    print(result.summary())
