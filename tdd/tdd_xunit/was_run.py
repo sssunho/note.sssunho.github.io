9d7241f3a889a3d3d27f6a11ecbb2ef16178ca3d
sssunho <sssunho.lee@gmail.com>
2020-01-19 17:21:19 +0900

    TDD: 21장. 셈하기
    - 가짜 구현을 한 뒤에 단계적으로 상수를 변수로 바꾸어 실제 구현으로 만듦
    - 또 다른 테스트를 작성
    - 테스트가 실패했을 때 좀더 작은 스케일로 또 다른 테스트를 만들어서
    실패한 테스트가 성공하게 만드는 것을 보조할 수 있었다.

--- tdd/tdd_xunit/was_run.py
+++ tdd/tdd_xunit/was_run.py
@@ -4,8 +4,9 @@
 [O] 나중에 tearDown 호출하기
 [] 테스트 메서드가 실패하더라도 tearDown 호출하기
 [] 여러 개의 테스트 실행하기
-[] 수집된 결과를 출력하기
+[O] 수집된 결과를 출력하기
 [O] wasRun에 로그 문자열 남기기
+[] 실패한 테스트 보고하기 
 """
 
 
@@ -17,10 +18,15 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
+
         self.setup()
         method = getattr(self, self.name)
         method()
         self.teardown()
+
+        return result
 
     def teardown(self):
         pass
@@ -35,18 +41,43 @@ class WasRun(TestCase):
 
     def test_method(self):
         self.log = self.log + 'test_method '
-        # self.wasRun = 1
 
     def teardown(self):
         self.log = self.log + 'teardown '
+
+    def test_broken_method(self):
+        raise Exception
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def test_started(self):
+        self.runCount += 1
+
+    def summary(self):
+        return '{} run, 0 failed'.format(self.runCount)
 
 
 class TestCaseTest(TestCase):
     def test_template_method(self):
         test = WasRun('test_method')
         test.run()
-        assert 'setup test_method teatdown ' == test.log
+        assert 'setup test_method teardown ' == test.log
+
+    def test_result(self):
+        test = WasRun('test_method')
+        result = test.run()
+        assert '1 run, 0 failed' == result.summary()
+
+    def test_failed_result(self):
+        test = WasRun('test_broken_method')
+        result = test.run()
+        assert '1 run, 1 failed' == result.summary()
 
 
 if __name__ == '__main__':
-    TestCaseTest('test_template_method')
+    TestCaseTest('test_template_method').run()
+    TestCaseTest('test_result').run()
+    TestCaseTest('test_failed_result').run()
