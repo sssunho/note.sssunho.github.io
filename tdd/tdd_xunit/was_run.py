2a0f8b9b8a0046b58c493484a00ec0275ba3d8ca
sssunho <sssunho.lee@gmail.com>
2020-01-19 16:45:38 +0900

    TDD: 19장. 뒷정리하기
    - 플래그에서 로그로 테스트 전략을 구조 조정
    - 새로운 로그 기능을 이용하여 tearDown()을 테스트하고 구현
    - 문제를 발견했는데 뒤로 돌아가는 대신 과감히 수정(잘한 일일까?)

--- tdd/tdd_xunit/was_run.py
+++ tdd/tdd_xunit/was_run.py
@@ -1,10 +1,11 @@
 """
-테스트 메서드 호출하기
-먼저 setUp 호출하기
-나중에 tearDown 호출하기
-테스트 메서드가 실패하더라도 tearDown 호출하기
-여러 개의 테스트 실행하기
-수집된 결과를 출력하기
+[O] 테스트 메서드 호출하기
+[O] 먼저 setUp 호출하기
+[O] 나중에 tearDown 호출하기
+[] 테스트 메서드가 실패하더라도 tearDown 호출하기
+[] 여러 개의 테스트 실행하기
+[] 수집된 결과를 출력하기
+[O] wasRun에 로그 문자열 남기기
 """
 
 
@@ -19,6 +20,10 @@ class TestCase:
         self.setup()
         method = getattr(self, self.name)
         method()
+        self.teardown()
+
+    def teardown(self):
+        pass
 
 
 class WasRun(TestCase):
@@ -26,30 +31,22 @@ class WasRun(TestCase):
         TestCase.__init__(self, name)
 
     def setup(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = 'setup '
 
     def test_method(self):
-        self.wasRun = 1
+        self.log = self.log + 'test_method '
+        # self.wasRun = 1
+
+    def teardown(self):
+        self.log = self.log + 'teardown '
 
 
 class TestCaseTest(TestCase):
-    def test_running(self):
+    def test_template_method(self):
         test = WasRun('test_method')
         test.run()
-        assert test.wasRun
-
-    def test_setup(self):
-        test = WasRun('test_method')
-        test.run()
-        assert test.wasSetUp
+        assert 'setup test_method teatdown ' == test.log
 
 
 if __name__ == '__main__':
-    TestCaseTest('test_running').run()
-    TestCaseTest('test_setup').run()
-
-    # test = WasRun('test_method')
-    # print(test.wasRun)
-    # test.run()  # 진짜 인터페이스
-    # print(test.wasRun)
+    TestCaseTest('test_template_method')
